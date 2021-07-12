8759dc486fd127519822f6d12f42ce319a906495
Hemlata-Bisariya <hemlata18oct@gmail.com>
2021-07-11 22:56:30 -0900

    some files added

--- Chat-App/src/utils/users.js
+++ Chat-App/src/utils/users.js
@@ -1,7 +1,5 @@
 const users = [];
 
-// addUser , removeUser , getUser , getUserInRoom
-
 const addUser = ({ id, username, room }) => {
   //Clean the data
   username = username.trim().toLowerCase();
@@ -32,6 +30,15 @@ const addUser = ({ id, username, room }) => {
   return { user };
 };
 
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
+};
+
 const removeUser = (id) => {
   const index = users.findIndex((user) => {
     return user.id === id;
@@ -47,9 +54,29 @@ addUser({
   username: "Hemlata Bisariya",
   room: "hello-User",
 });
+
+addUser({
+  id: 33,
+  username: "Tineshwar Bisariya",
+  room: "H&T",
+});
+
+addUser({
+  id: 44,
+  username: "Dimpy Bisariya",
+  room: "hello-User",
+});
+
+addUser({
+  id: 55,
+  username: "Jyoti Bisariya",
+  room: "H&T",
+});
+
 console.log(users);
 
-const removedUser = removeUser(22);
+const user = getUser(33);
+console.log(user);
 
-console.log(removedUser);
-console.log(users);
+const userList = getUsersInRoom("H&T");
+console.log(userList);
